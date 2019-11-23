bb67430cdf3bfba2fe274f2a70d2c68915e8935e
seungho Jeon <urijsh612@gmail.com>
2019-11-23 18:04:30 +0900

    #5.1 Container Presenter Pattern part Two

---
+++ src/Routes/Search/index.js
@@ -0,0 +1,3 @@
+import SearchContainer from "./DetailContainer";
+
+export default SearchContainer;
--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3",
@@ -6,46 +6,48 @@ const api = axios.create({
     api_key: "10923b261ba94d897ac6b81148314a3f",
     language: "ko-KR"
   }
-})
-
+});
 
 export const movieApi = {
   // url 맨앞에 "/"이거 주의 --> "/" 있을 시 절대경로로 인식되어, baseUrl을 덮어쓴다.
   nowPlaying: () => api.get("movie/now_playing"),
   upcoming: () => api.get("movie/upcoming"),
   popular: () => api.get("movie/popular"),
-  moiveDetail: id => api.get(`movie/${id}`, {
-    params: {
-      append_to_response: "videos"
-    }
-  }),
-  search: term => api.get("search/movie", {
-    params: {
-      query: encodeURIComponent(term)
-    }
-  })
-}
-
-
+  moiveDetail: id =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos"
+      }
+    }),
+  search: term =>
+    api.get("search/movie", {
+      params: {
+        query: encodeURIComponent(term)
+      }
+    })
+};
 
 export const tvApi = {
-  topRated: () => api.get("tv/top_rated", {
-    params: {
-      //append_to_response --> 이것을 통해 서브 요청가능
-      //videos --> 예고편을 가져올 수 있다.
-      append_to_response: "videos"
-    }
-  }),
+  topRated: () =>
+    api.get("tv/top_rated", {
+      params: {
+        //append_to_response --> 이것을 통해 서브 요청가능
+        //videos --> 예고편을 가져올 수 있다.
+        append_to_response: "videos"
+      }
+    }),
   popular: () => api.get("tv/popular"),
   airingToday: () => api.get("tv/airing_today"),
-  showDetail: id => api.get(`tv/${id}`, {
-    params: {
-      append_to_response: "videos"
-    }
-  }),
-  search: term => api.get("search/tv", {
-    params: {
-      query: encodeURIComponent(term)
-    }
-  })
-}
+  showDetail: id =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos"
+      }
+    }),
+  search: term =>
+    api.get("search/tv", {
+      params: {
+        query: encodeURIComponent(term)
+      }
+    })
+};
