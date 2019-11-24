95923fa6b0fd07a79ca737e7da58d761a836d205
seungho Jeon <urijsh612@gmail.com>
2019-11-24 23:50:45 +0900

    6.1 HomePresenter and Section Components

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,9 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "../../Components/Section";
 
-const HomePresenter = ({ nowPlaying, popular, upcomfing, loading, error }) =>
-  "Home";
+const Container = styled.div`
+  padding: 0px, 10px;
+`;
+
+const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
+  loading ? null : (
+    <Container>
+      {/* && 연산자를 이용하여 조건을 체크한다
+      - true && true && 10  // 출력값 → 10
+      - true && false && 10  // 출력값 → false */}
+
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map(movie => movie.title)}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Movies">
+          {upcoming.map(movie => movie.title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Movies">
+          {popular.map(movie => movie.title)}
+        </Section>
+      )}
+    </Container>
+  );
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
