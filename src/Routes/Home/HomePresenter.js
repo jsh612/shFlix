e28a5d33703b9b04d8c9672c0285ed93abecb3b0
seungho Jeon <urijsh612@gmail.com>
2019-11-26 16:17:17 +0900

    #6.6 Rendering Poster Component

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -7,7 +7,7 @@ import Message from "../../Components/Message";
 import Poster from "../../Components/Poster";
 
 const Container = styled.div`
-  padding: 0px, 20px;
+  padding: 0px 20px;
 `;
 
 const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
@@ -22,21 +22,51 @@ const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
           {nowPlaying.map(movie => (
-            <Poster />
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              imageUrl={movie.poster_path}
+              title={movie.original_title}
+              rating={movie.vote_average}
+              year={
+                movie.release_date ? movie.release_date.substring(0, 4) : ""
+              }
+              isMovie={true}
+            />
           ))}
         </Section>
       )}
       {upcoming && upcoming.length > 0 && (
         <Section title="Upcoming Movies">
           {upcoming.map(movie => (
-            <Poster key={movie.id} title={movie.title} />
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              imageUrl={movie.poster_path}
+              title={movie.original_title}
+              rating={movie.vote_average}
+              year={
+                movie.release_date ? movie.release_date.substring(0, 4) : ""
+              }
+              isMovie={true}
+            />
           ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Movies">
           {popular.map(movie => (
-            <Poster />
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              imageUrl={movie.poster_path}
+              title={movie.original_title}
+              rating={movie.vote_average}
+              year={
+                movie.release_date ? movie.release_date.substring(0, 4) : ""
+              }
+              isMovie={true}
+            />
           ))}
         </Section>
       )}
